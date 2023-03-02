4dbceea3a81170f02720a0583d7ece6f52439529
thebhangarwale devapps <thebhangarwaledevapps@gmail.com>
2023-03-02 12:58:13 +0530

    new commit

--- src/test/java/com/app/request/repository/RepositoryImplTest.java
+++ src/test/java/com/app/request/repository/RepositoryImplTest.java
@@ -1,34 +1,73 @@
 package com.app.request.repository;
 
-import com.app.request.datasource.local.DatabaseDataSource;
+import com.app.request.datasource.network.NetworkDataSource;
+import com.app.request.entity.BhangarTypeAndPrice;
+import com.app.request.entity.Media;
+import com.app.request.entity.RequestItem;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import static org.mockito.BDDMockito.given;
 
 @SpringBootTest
 class RepositoryImplTest {
 
     @Autowired
-    private DatabaseDataSource databaseDataSourceImpl;
+    private RepositoryImpl repository;
+
+    @MockBean
+    private NetworkDataSource networkDataSourceImpl;
 
     @Test
-    void test_deleteMedia(){
-        try {
-            long count= databaseDataSourceImpl.deleteMediaByCustomerIdRequestIdAndMediaId(
-                    "Bhangarwale_00001",
-                    3,
-                    Arrays.asList(
-                            "AOqewoI1PbFp9l9iSwA5KOLZQ2F4AIIn5qvM2P39dIhDw_UrO7JZMrTytoz6gFANFtBVM0NW1xeGXHYD4Irr6fyeQJQN_XMSFg",
-                            "AOqewoJ_DJv0I0Y7X0yHM6Yl7B5jOigm64PpZ0MIOeDOTBPPEyR-HzVOsc23sI2m90Sg_t3whuAq-cEVKwtGPWoeCSo4ObgjSQ"
-                    )
-            );
-            System.out.println(count);
+    void addRequestInBucket() throws Exception{
+        given(networkDataSourceImpl.getBhangarItemInfo(1L)).willReturn(new BhangarTypeAndPrice(
+                1L,
+                "loha",
+                "kg",
+                33.3
+        ));
 
-        }catch (Exception exception){
-            System.out.println(exception);
-        }
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        String resourceName1 = "dr._A.P.J.Abdul_Kalam.jpeg";
+        File file1 = new File(classLoader.getResource(resourceName1).getFile());
+        MultipartFile multipartFile1 = new MockMultipartFile(resourceName1, resourceName1, "image/jpeg", new FileInputStream(file1));
+
+        String resourceName2 = "mahatma_gandhi.jpeg";
+        File file2 = new File(classLoader.getResource(resourceName2).getFile());
+        MultipartFile multipartFile2 = new MockMultipartFile(resourceName2, resourceName2, "image/jpeg", new FileInputStream(file2));
+
+        String resourceName3 = "shree_ram.jpeg";
+        File file3 = new File(classLoader.getResource(resourceName3).getFile());
+        MultipartFile multipartFile3 = new MockMultipartFile(resourceName3, resourceName3, "image/jpeg", new FileInputStream(file3));
+
+        Boolean requestedAdded = repository.addRequestInBucket(
+                1L,
+                20,
+                (MultipartFile[]) Arrays.asList(multipartFile1, multipartFile2, multipartFile3).toArray(),
+                "user1"
+        );
+
+        Assertions.assertTrue(requestedAdded);
 
     }
 
+    @Test
+    void deleteMedia() {
+        List<RequestItem> requestItems = repository.getCustomerRequests("user1");
+        RequestItem requestItem = requestItems.get(0);
+        List<String> mediaIds = requestItem.getItem().getMedias().subList(1,2).stream().map(media -> media.getMediaId()).collect(Collectors.toList());
+        List<Media> mediaList = repository.deleteMedia("user1",requestItem.getRequestItemId(),mediaIds);
+        Assertions.assertEquals(2,mediaList.size());
+    }
 }
